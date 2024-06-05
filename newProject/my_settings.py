ab13857bd6dded066ecc65529c6a3b00b4a9ac04
iamjinwon <a67841586@gmail.com>
2024-06-05 11:30:01 +0900

    삭제된 settings파일 추가

---
+++ newProject/my_settings.py
@@ -0,0 +1,17 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'IT_news',
+        'USER' : 'teamlab',
+        'PASSWORD' : 'teamlab1234',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
+    }
+}
+
+SECRET_KEY = 'django-insecure-gddb)2@+aik@5uq#wh*=1dh-zs*s*x+@x3m4(#s3n0o#p4&+-d'
