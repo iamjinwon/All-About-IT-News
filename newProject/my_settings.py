bfe98df6573fea7f62a53b654c0f5993f5e9ffe5
iamjinwon <a67841586@gmail.com>
2024-06-10 19:42:01 +0900

    임시 비번 삭제

--- newProject/my_settings.py
+++ newProject/my_settings.py
@@ -3,7 +3,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'IT_news',
         'USER' : 'teamlab',
-        'PASSWORD' : 'teamlab1234',
+        'PASSWORD' : '',
         'HOST' : '127.0.0.1',
         'PORT' : '3306',
         'OPTIONS': {
