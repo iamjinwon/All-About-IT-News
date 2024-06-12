57a0534d83d0cb071f87a5060be43ac285699efd
iamjinwon <a67841586@gmail.com>
2024-06-12 21:38:40 +0900

    contact.html 수정

--- newApp/forms.py
+++ newApp/forms.py
@@ -1,7 +1,18 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import User
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['user_name', 'email']
+        labels = {
+            'user_name': '이름',
+            'email': '이메일',
+        }
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("이미 구독된 이메일입니다.")
+        return email
