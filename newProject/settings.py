147b40da77e18607d36a1ad84c714ebb1a8049f7
iamjinwon <a67841586@gmail.com>
2024-06-28 06:47:01 +0900

    static root 추가

--- newProject/settings.py
+++ newProject/settings.py
@@ -89,6 +89,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'newApp' / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
