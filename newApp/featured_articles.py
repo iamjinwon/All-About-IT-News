f6b86805792096340e31e8f1f34dcf5583696eef
iamjinwon <a67841586@gmail.com>
2024-07-05 18:48:52 +0900

    batch로 수정

--- newApp/featured_articles.py
+++ newApp/featured_articles.py
@@ -3,9 +3,12 @@ from dotenv import load_dotenv
 import os
 import django
 import sys
-from datetime import datetime, time
+from datetime import datetime,time
 import pytz
+import json
+import time as time_module
 from system_prompt import system_prompt1
+
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 project_path = os.path.join(current_path, '..')
@@ -20,25 +23,84 @@ from newApp.models import News, Gpt
 dotenv_path = os.path.join(project_path, '.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-def make_output(system_prompt, content):
-    client = OpenAI(
-        api_key=os.getenv("OPENAI_API_KEY")
+
+def fetch_crucial_news():
+    seoul_tz = pytz.timezone('Asia/Seoul')
+    now = datetime.now(seoul_tz).replace(tzinfo=None)
+    today_start = datetime.combine(now.date(), time.min).replace(tzinfo=None)
+    today_end = datetime.combine(now.date(), time.max).replace(tzinfo=None)
+
+    news_data = News.objects.filter(created_dt__range=(today_start, today_end)).values_list('news_id', 'title')
+    if not news_data:
+        print("No news articles found for today's date.")
+        return [], 0
+    
+    jsonl_filename = os.path.join(current_path, 'batchinput.jsonl')
+
+    with open(jsonl_filename, 'w', encoding='utf-8') as file:
+        json_entry = {
+            "custom_id": f"news-0",
+            "method": "POST", 
+            "url": "/v1/chat/completions",
+            "body" :{
+                "model": "gpt-4o",
+                "messages": [
+                    {"role": "system", "content": system_prompt1},
+                    {"role": "user", "content": "\n".join([f"{news_id}: {title}" for news_id, title in news_data])}
+                ],
+                "temperature": 0.7
+                }
+            }
+        file.write(json.dumps(json_entry, ensure_ascii=False) + '\n')
+
+def make_output():
+    fetch_crucial_news()
+    api_key = os.getenv("OPENAI_API_KEY")
+    client = OpenAI(api_key=api_key)
+
+    batch_input_file = client.files.create(
+        file=open("newApp/batchinput.jsonl", "rb"),
+        purpose="batch"
+    )
+    batch_input_file_id = batch_input_file.id
+
+    batch_job = client.batches.create(
+        input_file_id=batch_input_file_id,
+        endpoint="/v1/chat/completions",
+        completion_window="24h"
     )
 
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": content},
-        ],
-        temperature=0.7
-    )
+    while True:
+        batch_job = client.batches.retrieve(batch_job.id)
+        if batch_job.status == "completed":
+            break
+        time_module.sleep(180)
+    
+    result_file_id = batch_job.output_file_id
+    result = client.files.content(result_file_id).content
+    print(result)
 
-    result = response.choices[0].message.content.strip()
-    input_tokens = response.usage.prompt_tokens
-    output_tokens = response.usage.completion_tokens
-    total_tokens = response.usage.total_tokens
-    return result, input_tokens, output_tokens, total_tokens
+    result_file_name = "./test_result.jsonl"
+
+    with open(result_file_name, 'wb') as file:
+        file.write(result)
+    # Loading data from saved file
+    results = []
+    with open(result_file_name, 'r') as file:
+        for line in file:
+            # Parsing the JSON string into a dict and appending to the list of results
+            json_object = json.loads(line.strip())
+            results.append(json_object)
+
+    # Reading only the first results
+    res = results[0]
+    res_body = res['response']['body']
+    res_content = res_body['choices'][0]['message']['content']
+    input_tokens = res_body['usage']['prompt_tokens']
+    output_tokens = res_body['usage']['completion_tokens']
+    total_tokens = res_body['usage']['total_tokens']
+    
+    return res_content, input_tokens, output_tokens, total_tokens
 
 def calculate_cost(model, input_tokens, output_tokens):
     if model == "gpt-4o":
@@ -48,22 +110,10 @@ def calculate_cost(model, input_tokens, output_tokens):
         return cost
     else:
         raise ValueError("Unsupported model")
-
+    
 def update_crucial_articles():
-    seoul_tz = pytz.timezone('Asia/Seoul')
-    now = datetime.now(seoul_tz).replace(tzinfo=None) 
-    today_start = datetime.combine(now.date(), time.min).replace(tzinfo=None)
-    today_end = datetime.combine(now.date(), time.max).replace(tzinfo=None) 
 
-    news_data = News.objects.filter(created_dt__range=(today_start, today_end)).values_list('news_id', 'title')
-
-    if not news_data:
-        print("No news articles found for today's date.")
-        return [], 0
-
-    content = "\n".join([f"{news_id}: {title}" for news_id, title in news_data])
-
-    selected_news_ids_str, fe_input_tokens, fe_output_tokens, fe_total_tokens = make_output(system_prompt1, content)
+    selected_news_ids_str, fe_input_tokens, fe_output_tokens, fe_total_tokens = make_output()
 
     try:
         selected_news_ids = [int(news_id.strip()) for news_id in selected_news_ids_str.split(',')]
@@ -73,8 +123,8 @@ def update_crucial_articles():
         return [], fe_total_tokens
 
     # 비용 계산
-    fe_cost = calculate_cost("gpt-4o", fe_input_tokens, fe_output_tokens)
-    cost_won = fe_cost * 1300
+    fe_cost = (calculate_cost("gpt-4o", fe_input_tokens, fe_output_tokens)) / 2
+    cost_won = (fe_cost * 1300) / 2
 
     Gpt.objects.create(
         task="기사분류",
@@ -88,3 +138,6 @@ def update_crucial_articles():
     News.objects.filter(news_id__in=selected_news_ids).update(crucial=True)
 
     return selected_news_ids, fe_total_tokens
+
+if __name__ == "__main__":
+    update_crucial_articles()
--- newApp/summarize_gpt.py
+++ newApp/summarize_gpt.py
@@ -5,6 +5,8 @@ import django
 import sys
 from datetime import datetime, time
 import pytz
+import json
+import time as time_module
 from system_prompt import system_prompt2
 
 current_path = os.path.dirname(os.path.abspath(__file__))
@@ -20,30 +22,85 @@ from newApp.models import News, SummarizeNews, Gpt
 dotenv_path = os.path.join(project_path, '.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-def summarize_gpt(system_prompt, news_content):
-    client = OpenAI(
-        api_key=os.getenv("OPENAI_API_KEY")
+def fetch_crucial_news():
+    seoul_tz = pytz.timezone('Asia/Seoul')
+    now = datetime.now(seoul_tz).replace(tzinfo=None)
+    today_start = datetime.combine(now.date(), time.min).replace(tzinfo=None)
+    today_end = datetime.combine(now.date(), time.max).replace(tzinfo=None)
+
+    news_data = News.objects.filter(created_dt__range=(today_start, today_end), crucial=True)
+
+    if not news_data.exists():
+        print("No news articles found for today's date.")
+        return None, 0
+
+    jsonl_filename = os.path.join(current_path, 'batchinput.jsonl')
+    
+    with open(jsonl_filename, 'w', encoding='utf-8') as file:
+        for i, news in enumerate(news_data):
+            description = news.description
+            json_entry = {
+                "custom_id": f"news-{i}",
+                "method": "POST",
+                "url": "/v1/chat/completions",
+                "body": {
+                    "model": "gpt-4o",
+                    "messages": [
+                        {"role": "system", "content": system_prompt2},
+                        {"role": "user", "content": f"{description}"}
+                    ],
+                    "temperature": 0.7
+                }
+            }
+            file.write(json.dumps(json_entry, ensure_ascii=False) + "\n")
+
+    return news_data, len(news_data)
+
+def make_output():
+    api_key = os.getenv("OPENAI_API_KEY")
+    client = OpenAI(api_key=api_key)
+
+    batch_input_file_path = os.path.join(current_path, 'batchinput.jsonl')
+    batch_input_file = client.files.create(
+        file=open(batch_input_file_path, "rb"),
+        purpose="batch"
+    )
+    batch_input_file_id = batch_input_file.id
+
+    batch_job = client.batches.create(
+        input_file_id=batch_input_file_id,
+        endpoint="/v1/chat/completions",
+        completion_window="24h"
     )
 
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": news_content},
-        ],
-        temperature=0.2
-    )
+    while True:
+        batch_job = client.batches.retrieve(batch_job.id)
+        if batch_job.status == "completed":
+            break
+        time_module.sleep(600)  # 10분 대기
 
-    result = response.choices[0].message.content
-    input_tokens = response.usage.prompt_tokens
-    output_tokens = response.usage.completion_tokens 
-    total_tokens = response.usage.total_tokens 
-    return result, input_tokens, output_tokens, total_tokens
+    result_file_id = batch_job.output_file_id
+    result = client.files.content(result_file_id).content
+    print(result)
+
+    result_file_name = os.path.join(current_path, 'test_result.jsonl')
+
+    with open(result_file_name, 'wb') as file:
+        file.write(result)
+
+    # 저장된 파일에서 데이터 로드
+    results = []
+    with open(result_file_name, 'r') as file:
+        for line in file:
+            json_object = json.loads(line.strip())
+            results.append(json_object)
+
+    return results
 
 def calculate_cost(model, input_tokens, output_tokens):
     if model == "gpt-4o":
-        input_cost_per_token = 5 / 1_000_000 
-        output_cost_per_token = 15 / 1_000_000 
+        input_cost_per_token = 5 / 1_000_000
+        output_cost_per_token = 15 / 1_000_000
         cost = (input_cost_per_token * input_tokens) + (output_cost_per_token * output_tokens)
         return cost
     else:
@@ -55,23 +112,29 @@ def summarize_articles():
     today_start = datetime.combine(now.date(), time.min).replace(tzinfo=None)
     today_end = datetime.combine(now.date(), time.max).replace(tzinfo=None)
 
-    news_data = News.objects.filter(created_dt__range=(today_start, today_end), crucial=True)
+    news_data, news_count = fetch_crucial_news()
 
-    if not news_data.exists():
+    if not news_data:
         print("No news articles found for today's date.")
         return
 
-    for news in news_data:
+    results = make_output()
+
+    for i, news in enumerate(news_data):
         description = news.description
         news_id = news.news_id
+        res = results[i]
+        res_body = res['response']['body']
+        res_content = res_body['choices'][0]['message']['content']
+        input_tokens = res_body['usage']['prompt_tokens']
+        output_tokens = res_body['usage']['completion_tokens']
+        total_tokens = res_body['usage']['total_tokens']
 
-        summarize, input_tokens, output_tokens, total_tokens = summarize_gpt(system_prompt2, description)
-        cost = calculate_cost("gpt-4o", input_tokens, output_tokens)
-        cost_won = cost * 1300
+        cost = calculate_cost("gpt-4o", input_tokens, output_tokens) / 2
+        cost_won = (cost * 1300) / 2
 
-        if summarize:
-            summary_lines = summarize.strip().split('\n')
-            
+        if res_content:
+            summary_lines = res_content.strip().split('\n')
             if len(summary_lines) >= 3:
                 summary_dict = {
                     "news_id": news_id,
@@ -89,7 +152,7 @@ def summarize_articles():
                     print(f"Created new summary for news_id {news_id}")
                 else:
                     print(f"Updated existing summary for news_id {news_id}")
-                
+
                 Gpt.objects.create(
                     task="기사요약",
                     input_tokens=input_tokens,
