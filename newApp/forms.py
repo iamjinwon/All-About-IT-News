632145fb31f55bdcf3eddf28b1c6aebda0e33411
iamjinwon <a67841586@gmail.com>
2024-07-06 20:28:10 +0900

    form 수정

--- newApp/forms.py
+++ newApp/forms.py
@@ -24,13 +24,14 @@ class UserForm(forms.ModelForm):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        if email:
-            try:
-                validate_email(email)
-            except ValidationError:
-                raise ValidationError("유효하지 않은 이메일 형식입니다.")
-
-            if User.objects.filter(email=email).exists():
-                raise ValidationError("이미 구독된 이메일입니다.")
+        if not email:
+            raise ValidationError("이메일을 입력해주세요.")
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValidationError("유효하지 않은 이메일 형식입니다.")
+        
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("이미 구독된 이메일입니다.")
 
         return email
